4bffe6ef4424152fbec6d2aed808182375d57fcc
giampaolli <giampaoli2011@gmail.com>
2019-02-25 16:38:08 -0300

    send email and create queue

---
+++ src/config/mail.js
@@ -0,0 +1,9 @@
+module.exports = {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '870b23e1c8c8e9',
+    pass: '075e7cab85052c'
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -11,10 +11,18 @@ routes.post('/sessions', controllers.SessionController.store)
 
 routes.use(authMiddleware)
 
+/**
+ * Add routes
+ */
 routes.get('/ads', controllers.AdController.index)
 routes.get('/ads/:id', controllers.AdController.show)
 routes.post('/ads', controllers.AdController.store)
 routes.put('/ads/:id', controllers.AdController.update)
 routes.delete('/ads/:id', controllers.AdController.destroy)
+
+/**
+ * Prochase routes
+ */
+routes.post('/porchases', controllers.PurchaseController.store)
 
 module.exports = routes
